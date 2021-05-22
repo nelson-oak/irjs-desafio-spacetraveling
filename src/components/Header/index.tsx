18f87475d186f605e3778c165e724c71063adced
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-22 12:59:31 -0300

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Logo" />
+      </div>
+    </div>
+  );
 }
